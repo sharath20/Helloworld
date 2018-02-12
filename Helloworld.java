28b4db05ea03b59bb093125f672d53aaf654d072
Sharath chandra <mvcsharathsai95@gmail.com>
2018-02-12 16:27:34 -0600

    checked and edited to correct syntax

--- Helloworld.java
+++ Helloworld.java
@@ -1,6 +1,6 @@
-import.java.io.*;
+import java.io.*;
 public class Helloworld{
-public static void main(string args []){
+public static void main(String args []){
 System.out.println("Hello World");
 }
 }
